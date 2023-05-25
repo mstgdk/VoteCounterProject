00fc97c2956014c3270f7a28201ccbabcbbc9ea2
mstgdk <mstgdk@windowslive.com>
2023-05-25 21:51:51 +0300

    25052023

---
+++ src/main/java/com/votecounter/VotecounterApplication.java
@@ -0,0 +1,13 @@
+package com.votecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotecounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotecounterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/votecounter/controller/UserJwtController.java
@@ -0,0 +1,4 @@
+package com.votecounter.controller;
+
+public class UserJwtController {
+}
